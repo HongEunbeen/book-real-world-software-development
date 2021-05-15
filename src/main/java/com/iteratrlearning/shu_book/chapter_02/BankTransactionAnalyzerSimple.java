052a69ea91cbed63bc1d1cf084be504de8d4f15c
HongEunbeen <37995685+HongEunbeen@users.noreply.github.com>
2021-05-15 17:59:39 +0900

    입출금 내역 목록 처리 - 거래 목록 처리해 결과 반환 메서드 추가

--- src/main/java/com/iteratrlearning/shu_book/chapter_02/BankTransactionAnalyzerSimple.java
+++ src/main/java/com/iteratrlearning/shu_book/chapter_02/BankTransactionAnalyzerSimple.java
@@ -7,6 +7,7 @@ import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BankTransactionAnalyzerSimple {
@@ -23,8 +24,27 @@ public class BankTransactionAnalyzerSimple {
         final List<BankTransaction> bankTransactions = bankStatementParser.parseLinesFromCSV(lines);
 
         System.out.println("The total fro all transactions is " + calculateTotalAmount(bankTransactions));
-        System.out.println("Transactions in January " + selectInMonth(BankTransactions, Month.JANUARY));
+        System.out.println("Transactions in January " + selectInMonth(bankTransactions, Month.JANUARY));
 
 
+    }
+
+    public static double calculateTotalAmount(final List<BankTransaction> bankTransactions){
+        double total = 0d;
+        for(final BankTransaction bankTransaction : bankTransactions) {
+            total += bankTransaction.getAmount();
+        }
+        return total;
+    }
+
+    public static List<BankTransaction> selectInMonth(List<BankTransaction> bankTransactions, final Month month){
+        final List<BankTransaction> bankTransactionsInMonth = new ArrayList<>();
+
+        for(final BankTransaction bankTransaction : bankTransactions){
+            if(bankTransaction.getDate().getMonth() == month){
+                bankTransactionsInMonth.add(bankTransaction);
+            }
+        }
+        return bankTransactionsInMonth;
     }
 }
