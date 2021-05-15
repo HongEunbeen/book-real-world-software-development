7371034b6eb6078f2da2584c1bd6f14839cfddc5
HongEunbeen <37995685+HongEunbeen@users.noreply.github.com>
2021-05-15 17:41:24 +0900

    파싱 로직 추출해 한 클래스로 만듦

--- src/main/java/com/iteratrlearning/shu_book/chapter_02/BankStatementCSVParser.java
+++ src/main/java/com/iteratrlearning/shu_book/chapter_02/BankStatementCSVParser.java
@@ -1,9 +1,29 @@
 package com.iteratrlearning.shu_book.chapter_02;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BankStatementCSVParser {
     final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
-    private BankTransaction paresFromCSV
+    private BankTransaction paresFromCSV(final String line){
+        final String[] columns = line.split(",");
+
+        final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+        final double amount = Double.parseDouble(columns[1]);
+        final String description =columns[2];
+
+        return new BankTransaction(date, amount, description);
+    }
+
+    public List<BankTransaction> parseLinesFromCSV(final List<String> lines){
+        final List<BankTransaction> bankTransactions = new ArrayList<>();
+        for(final String line : lines){
+            bankTransactions.add(paresFromCSV(line));
+        }
+        return bankTransactions;
+    }
 }
