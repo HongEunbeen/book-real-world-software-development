0b7e4ec10c6352d0822e3e817386eeb6ef64dd10
HongEunbeen <37995685+HongEunbeen@users.noreply.github.com>
2021-05-22 15:40:38 +0900

    Revert "3장 기본 설정"
    This reverts commit f2e3ac2d7fba07952c03da4088fa85e1bce7162f.

--- src/main/java/com/iteratrlearning/shu_book/chapter_03/BankStatementParser.java
+++
@@ -1,8 +0,0 @@
-package com.iteratrlearning.shu_book.chapter_03;
-
-import java.util.List;
-
-public interface BankStatementParser {
-    BankTransaction parserFrom(String line);
-    List<BankTransaction> parserLinesFrom(List<String> lines);
-}
