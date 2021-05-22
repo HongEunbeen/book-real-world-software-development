cbe1f26e6ae2440f06ec83fc2e193c49f4ad8aac
HongEunbeen <37995685+HongEunbeen@users.noreply.github.com>
2021-05-22 15:47:07 +0900

    특정 월이나 금액으로 입출금 내역 검색하기

--- src/main/java/com/iteratrlearning/shu_book/chapter_03/BankStatementProcessor.java
+++ src/main/java/com/iteratrlearning/shu_book/chapter_03/BankStatementProcessor.java
@@ -42,7 +42,7 @@ public class BankStatementProcessor {
     public  List<BankTransaction> findTransactionsGreaterThanEqula(final int amount){
         final List<BankTransaction> result = new ArrayList<>();
         for(final BankTransaction bankTransaction : bankTransactions){
-            if(bankTransaction.getAmount() >= = amount){
+            if(bankTransaction.getAmount() >= amount){
                 result.add(bankTransaction);
             }
         }
@@ -53,6 +53,16 @@ public class BankStatementProcessor {
         final List<BankTransaction> result = new ArrayList<>();
         for(final BankTransaction bankTransaction : bankTransactions){
             if(bankTransaction.getDate().getMonth() == month){
+                result.add(bankTransaction);
+            }
+        }
+        return result;
+    }
+
+    public List<BankTransaction> findTransactionsInMonthAndBGreater(final Month month, final int amount){
+        final List<BankTransaction> result = new ArrayList<>();
+        for(final BankTransaction bankTransaction : bankTransactions){
+            if(bankTransaction.getDate().getMonth() == month && (bankTransaction.getAmount() >= amount)){
                 result.add(bankTransaction);
             }
         }
