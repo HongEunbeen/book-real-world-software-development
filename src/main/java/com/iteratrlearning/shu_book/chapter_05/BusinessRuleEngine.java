0087bea5e51a9790a3be07361db87660f4895d12
HongEunbeen <vvvv980@gmail.com>
2021-06-20 03:07:41 +0900

    feat : Business규칙 기본 API 작성

---
+++ src/main/java/com/iteratrlearning/shu_book/chapter_05/BusinessRuleEngine.java
@@ -0,0 +1,17 @@
+package com.iteratrlearning.shu_book.chapter_05;
+
+
+
+public class BusinessRuleEngine {
+    public void addAction(final Action action){
+        throw new UnsupportedOperationException();
+    }
+
+    public void count(){
+        throw new UnsupportedOperationException();
+    }
+
+    public void run(){
+        throw new UnsupportedOperationException();
+    }
+}
