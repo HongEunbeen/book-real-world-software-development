64705c919387ea7250a5b848b913b0e364e5837a
HongEunbeen <vvvv980@gmail.com>
2021-06-28 16:20:20 +0900

    feat : 특정 거래의 예상치 계산 규칙 추가

--- src/test/java/com/iteratrlearning/shu_book/chapter_05/BusinessRuleEngineTest.java
+++ src/test/java/com/iteratrlearning/shu_book/chapter_05/BusinessRuleEngineTest.java
@@ -68,4 +68,36 @@ public class BusinessRuleEngineTest {
         verify(mockAction).perform(mockFacts);
 
     }
+
+    @Test
+    public void calcForecastedAmountWithFacts(){
+        //given
+        final Action mockAction = mock(Action.class);
+        final Facts mockFacts = mock(Facts.class);
+        final BusinessRuleEngine businessRuleEngine = new BusinessRuleEngine(mockFacts);
+
+        //when
+        businessRuleEngine.addAction(facts -> {
+            var forecastedAmount = 0.0;
+            var dealStage = Stage.valueOf(facts.getFacts("stage"));
+            var amount = Double.parseDouble(facts.getFacts("amount"));
+            if(dealStage == Stage.LEAD){
+                forecastedAmount = amount * 0.2;
+            }
+            else if(dealStage == Stage.EVALUATING){
+                forecastedAmount = amount * 0.5;
+            }
+            else if(dealStage == Stage.INTERESTED){
+                forecastedAmount = amount * 0.8;
+            }
+            else if(dealStage == Stage.CLOSE){
+                forecastedAmount = amount ;
+            }
+            facts.addFacts("forecastedAmount", String.valueOf(forecastedAmount));
+        });
+
+        //then
+        verify(mockAction).perform(mockFacts);
+
+    }
 }
