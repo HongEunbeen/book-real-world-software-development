5acca0e18f621d28b07b9f2bf5d681aeac8ca9e2
HongEunbeen <vvvv980@gmail.com>
2021-06-28 16:21:40 +0900

    refactor : switch 문을 이용해 규칙 리팩토링

--- src/test/java/com/iteratrlearning/shu_book/chapter_05/BusinessRuleEngineTest.java
+++ src/test/java/com/iteratrlearning/shu_book/chapter_05/BusinessRuleEngineTest.java
@@ -81,17 +81,15 @@ public class BusinessRuleEngineTest {
             var forecastedAmount = 0.0;
             var dealStage = Stage.valueOf(facts.getFacts("stage"));
             var amount = Double.parseDouble(facts.getFacts("amount"));
-            if(dealStage == Stage.LEAD){
-                forecastedAmount = amount * 0.2;
-            }
-            else if(dealStage == Stage.EVALUATING){
-                forecastedAmount = amount * 0.5;
-            }
-            else if(dealStage == Stage.INTERESTED){
-                forecastedAmount = amount * 0.8;
-            }
-            else if(dealStage == Stage.CLOSE){
-                forecastedAmount = amount ;
+            switch (dealStage){
+                case LEAD:
+                    forecastedAmount = amount * 0.2; break;
+                case EVALUATING:
+                    forecastedAmount = amount * 0.5; break;
+                case INTERESTED:
+                    forecastedAmount = amount * 0.8; break;
+                case CLOSE:
+                    forecastedAmount = amount; break;
             }
             facts.addFacts("forecastedAmount", String.valueOf(forecastedAmount));
         });
